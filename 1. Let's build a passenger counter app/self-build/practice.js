eae2dd97fc3068a565e1570d3a5fecea4a54bb05
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-04 10:44:19 +0500

    commit initalized

---
+++ 1. Let's build a passenger counter app/self-build/practice.js
@@ -0,0 +1,74 @@
+// ----------------------
+// Practice:
+
+// let firstBatch = 5
+// let secondBatch = 10
+
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+
+
+// ==== 1st Challenge:
+
+// let myAge = 19
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+// ----------------------
+// Practice:
+
+// let count = 5
+
+// count = count + 1
+// count = count + 10
+// count = count - 8
+
+// console.log(count)
+
+// ==== 2nd Challenge
+
+// let bonusPoints = 50
+
+// bonusPoints = bonusPoints + 50
+// bonusPoints = bonusPoints - 75
+// bonusPoints = bonusPoints + 45
+
+// console.log(bonusPoints)
+
+// ==== Functions
+
+// function countDown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+// countDown()
+
+// ==== Challenge
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function lapSum() {
+//     console.log(lap1 + lap2 + lap3)
+// }
+// lapSum()
+
+// ==== Another challenge 
+
+let lapsCompleted = 0
+
+function lapsIncrement() {
+    lapsCompleted = lapsCompleted + 1
+    console.log(lapsCompleted)
+}
+
+lapsIncrement()
+lapsIncrement()
+lapsIncrement()
