099cd54586478b27394f90a92e8b0d679e7f0be2
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-05 20:23:14 +0500

    if else

---
+++ 2. Black Jack/myPractice/practice.js
@@ -0,0 +1,20 @@
+// let firstCard = 12
+// let secondCard = 9
+
+// let sum = firstCard + secondCard
+
+// if (sum < 21) {
+//     console.log("Do you want to draw a new card?")
+// } else if (sum === 21) {
+//     console.log("Wohoo! You've got the Black Jack.")
+// } else {
+//     console.log("You are out of the Game! ")
+// }
+
+let age = 22
+
+if (age <= 21) {
+    console.log("You are not allowed to enter in the club")
+} else {
+    console.log("You are allowed to enter in this club")
+}
