c12b8ce86543b5fb0f78df152abf7a81783c9209
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-06 22:40:25 +0500

    more on arrays

--- 2. Black Jack/black jack game/practice.js
+++ 2. Black Jack/black jack game/practice.js
@@ -5,3 +5,29 @@ let myProfile = [
     "Passion: Software Engineer"
 ]
 console.log(myProfile)
+console.log(myProfile.length)
+
+let mySelf = [
+    "Muhammad Ali",
+    19,
+    true
+]
+console.log(mySelf)
+
+let card = [1, 2, 3, 4]
+card.push(5)
+console.log(card)
+
+let messages = [
+    "hey, how's it going?",
+    "I'm great, thank you! How about you?",
+    "All good. Been working on my portfolio lately."
+]
+
+let newMsg = "Same here."
+
+messages.push(newMsg)
+console.log(messages)
+
+messages.pop()
+console.log(messages)
