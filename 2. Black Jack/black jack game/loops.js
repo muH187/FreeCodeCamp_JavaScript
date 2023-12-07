2b4e5c9cdedfbe1a65fd0db1febe6036df822e08
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-07 11:28:12 +0500

    done challenge

--- 2. Black Jack/black jack game/loops.js
+++ 2. Black Jack/black jack game/loops.js
@@ -9,3 +9,11 @@ for ( count = 10; count < 21; count += 1) {
 for ( count = 10; count < 101; count += 10) {
     console.log(count)
 }
+
+// Challenge:
+
+let cards = [7, 3, 9, true, false, "Karachi"]
+
+for (let i = 0; i < cards.length; i++) {
+    console.log(cards[i])
+}
