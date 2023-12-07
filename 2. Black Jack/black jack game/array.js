fce3093e660d9e6381c48d62df4601e0bdeb8170
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-07 11:23:39 +0500

    array with loops

--- 2. Black Jack/black jack game/array.js
+++ 2. Black Jack/black jack game/array.js
@@ -19,7 +19,7 @@ card.push(5)
 console.log(card)
 
 let messages = [
-    "hey, how's it going?",
+    "Hey, how's it going?",
     "I'm great, thank you! How about you?",
     "All good. Been working on my portfolio lately."
 ]
@@ -27,7 +27,12 @@ let messages = [
 let newMsg = "Same here."
 
 messages.push(newMsg)
+messages.push("Great to hear that.", "So where do you get now?", "I have almost built it from the scratch. What about you?", "I have build it and I'm just about to publish  it online.")
 console.log(messages)
 
-messages.pop()
-console.log(messages)
+// messages.pop()
+// console.log(messages)
+
+for ( let i = 0; i < messages.length; i += 1) {
+    console.log(messages[i])
+}
