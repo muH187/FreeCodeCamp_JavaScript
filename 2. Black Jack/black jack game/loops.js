14da5358e48cc93411c411450ae846b310e5063b
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-07 11:12:59 +0500

    more on loops

--- 2. Black Jack/black jack game/loops.js
+++ 2. Black Jack/black jack game/loops.js
@@ -5,3 +5,7 @@ for (count = 1; count < 11; count += 1) {
 for ( count = 10; count < 21; count += 1) {
     console.log(count)
 }
+
+for ( count = 10; count < 101; count += 10) {
+    console.log(count)
+}
