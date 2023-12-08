d37396760a6ad2e7156e7144196548c729f88f20
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-08 22:44:13 +0500

    logical operators

---
+++ 2. Black Jack/Logical Operator/.js
@@ -0,0 +1,10 @@
+let hasCompletedCourse = true
+let givesCertificate = true
+
+if (hasCompletedCourse === true && givesCertificate === false) {
+    generateCertificate()
+}
+
+function generateCertificate() {
+    console.log("Generating Certificate...")
+}
