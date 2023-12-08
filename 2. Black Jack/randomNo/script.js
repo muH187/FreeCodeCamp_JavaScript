176e332f75e685a32241db09bc8a86fd42f5fc99
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-08 21:45:53 +0500

    random number challenge

--- 2. Black Jack/randomNo/script.js
+++ 2. Black Jack/randomNo/script.js
@@ -1,3 +1,17 @@
-let randomNumber = Math.random()
+let randomNumber2 = Math.random() * 6
+console.log(randomNumber2)
 
+let flooredNumber = Math.floor(3.9251)
+console.log(flooredNumber)
+
+let randomNumber = Math.floor( Math.random() * 6 ) + 1
 console.log(randomNumber)
+
+// Challenge:
+
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1
+}
+
+let result = rollDice()
+console.log(result)
