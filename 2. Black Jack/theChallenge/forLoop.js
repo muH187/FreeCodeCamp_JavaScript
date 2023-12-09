d3737d8d842b5e5aab80d90d217c80f2d33ecbfa
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-09 21:04:50 +0500

    update

--- 2. Black Jack/theChallenge/forLoop.js
+++ 2. Black Jack/theChallenge/forLoop.js
@@ -1,5 +1,6 @@
 let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"]
 
+console.log("Top 5 largest countries in the world:")
 for (i = 0; i < largeCountries.length; i++) {
     console.log("-- " + largeCountries[i])
 }
