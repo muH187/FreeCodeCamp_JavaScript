1e532d637a5321d99212e3379e5f7837e351e9fd
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-09 21:28:22 +0500

    push pop shift unshift

---
+++ 2. Black Jack/theChallenge/pushPop.js
@@ -0,0 +1,9 @@
+let largeCountries = ["Tuvalu", "India", "USA", "Indonesia", "Monacco"]
+
+largeCountries.pop()
+largeCountries.push("Pakistan")
+
+largeCountries.shift()
+largeCountries.unshift("China")
+
+console.log(largeCountries)
