07870f967e81fb7d6132f1c07fe651b90f820d82
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-09 12:48:29 +0500

    more on logical operators with functions

---
+++ 2. Black Jack/Logical Operator/script.js
@@ -0,0 +1,21 @@
+let hasCompletedCourse = true
+let givesCertificate = true
+
+if (hasCompletedCourse === true && givesCertificate === false) {
+    generateCertificate()
+}
+
+function generateCertificate() {
+    console.log("Generating Certificate...")
+}
+// ======================================================
+let hasSolvedChallenge = false
+let hasHintLeft = false
+
+if (hasSolvedChallenge === false && hasHintLeft === false) {
+    showSolution()
+}
+
+function showSolution() {
+    console.log("Showing  the solutions...")
+}
