c3c8c458736437690552bf81fa8c19f143467ace
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-09 20:31:19 +0500

    obj and func. challenge

---
+++ 2. Black Jack/theChallenge/obj&func.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Muhammad Ali",
+    age: 19,
+    country: "Pakistan"
+}
+
+function logData() {
+    return person.name + " is " + person.age + " years old and lives in " + person.country
+}
+
+console.log(logData())
