88402dc322838c266456beefcfb66c4d3365fabe
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-09 16:04:38 +0500

    objects practice

---
+++ 2. Black Jack/Objects/.js
@@ -0,0 +1,24 @@
+let courseGrid = {
+    title: "Learn CSS Grid for free",
+    lessons: "16 lessons",
+    legnth: "1 Hour",
+    creator: "Muhammad Ali",
+    level: "Intermediate",
+    isFree: true,
+    tags: ["HTML", "CSS"]
+
+}
+
+console.log(courseGrid.tags)
+
+// ==================================================
+
+let castle = {
+    title: "Live like a king in my Castle",
+    price: 190,
+    specs: ["2 Bedrooms", "1 Drawing room"],
+    isLive: true,
+}
+
+console.log(castle.price)
+console.log(castle.specs)
