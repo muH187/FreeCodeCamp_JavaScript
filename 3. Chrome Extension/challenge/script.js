9934ca3e6944565d620cbbad23445852125e4009
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-11 19:59:31 +0500

    added challenge folder

---
+++ 3. Chrome Extension/challenge/script.js
@@ -0,0 +1,7 @@
+let containerEl = document.getElementById("container")
+
+containerEl.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+function buy() {
+    containerEl.innerHTML += "<p>Thank you for buying</p>"
+}
