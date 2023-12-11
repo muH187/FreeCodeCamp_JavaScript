f068c8436ff60a44974f169f1111ec8c99c180b3
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-11 14:05:44 +0500

    learning let and const

--- 3. Chrome Extension/chromeExtension/script.js
+++ 3. Chrome Extension/chromeExtension/script.js
@@ -3,3 +3,8 @@ let inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked from addEventListener")
 })
+
+let myLeads = []
+let inputEl = document.getElementById("input-el")
+
+inputEl.textContent = 
