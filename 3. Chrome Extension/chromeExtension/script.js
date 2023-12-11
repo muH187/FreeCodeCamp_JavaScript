d7bc70b7094687e783848acb7a09b6a236d15051
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-11 19:45:55 +0500

    for loop

--- 3. Chrome Extension/chromeExtension/script.js
+++ 3. Chrome Extension/chromeExtension/script.js
@@ -1,10 +1,16 @@
-let inputBtn = document.getElementById("input-btn")
+let myLeads = ["www.google.com", "www.facebook.com", "www.youtube.com"]
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener")
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
 
-let myLeads = []
-let inputEl = document.getElementById("input-el")
 
-inputEl.textContent = 
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
