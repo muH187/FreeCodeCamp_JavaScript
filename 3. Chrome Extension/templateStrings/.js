8145e9faa4269c112c75f8471dd1bd4e8e403879
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-12 11:21:14 +0500

    creating even more dynamic template string

--- 3. Chrome Extension/templateStrings/.js
+++ 3. Chrome Extension/templateStrings/.js
@@ -1,5 +1,6 @@
 const recipient = "Mirza"
+const sender = "Ali"
 
-const email = `Hey! ${recipient}, How's going. Cheers Ali`
+const email = `Hey! ${recipient}, How's going. Cheers ${sender}`
 
 console.log(email)
